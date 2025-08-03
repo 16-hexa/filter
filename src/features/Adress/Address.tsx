439c80e2edbbc1aac1086e488ccb9f2f1c7acf7c
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-08-03 11:57:38 +0300

    added new part

--- src/features/Adress/Address.tsx
+++ src/features/Adress/Address.tsx
@@ -3,7 +3,11 @@ import styles from './adress.module.scss'
 export const Address = () => {
     return(
         <div className={styles.container}>
+            <h2 className={styles.spots}>Наши споты</h2>
 
+            <div>
+
+            </div>
         </div>
     )
 }
