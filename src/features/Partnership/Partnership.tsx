6d8ccbf28409177af4c92421e41fd6ddd610eed2
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-08-05 12:01:20 +0300

    finished

--- src/features/Partnership/Partnership.tsx
+++ src/features/Partnership/Partnership.tsx
@@ -1,9 +1,40 @@
 import styles from './partnership.module.scss';
+import back from '@shared/images/back/back.png';
+import {useState} from "react";
 
 export const Partnership = () => {
-    return(
-        <div className={styles.container} id="part">
+    const [message, setMessage] = useState('');
 
+    const handleSubmit = () => {
+        console.log('Отправлено:', message);
+        setMessage('');
+    };
+
+    return (
+        <div className={styles.container} id="part">
+            <div className={styles.backgroundText}>Partnership</div>
+
+            <img
+                src={back}
+                alt="decorative line"
+                className={styles.back}
+            />
+
+            <div className={styles.formWrapper}>
+                <h3>Есть идеи,<br />с которыми ты готов поделиться?</h3>
+                <p>Напиши об этом!</p>
+
+                <textarea
+                    placeholder="Ваши идеи, предложения"
+                    className={styles.textarea}
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+
+                <button className={styles.button} onClick={handleSubmit}>
+                    Отправить
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
