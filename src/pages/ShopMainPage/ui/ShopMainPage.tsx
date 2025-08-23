143d618af6844d1ef45323aeb37c0ab80817262c
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-08-23 12:27:25 +0300

    feat: added new part

--- src/pages/ShopMainPage/ui/ShopMainPage.tsx
+++ src/pages/ShopMainPage/ui/ShopMainPage.tsx
@@ -1,20 +1,27 @@
 import styles from './shopMain.module.scss';
+import pht1 from '@shared/images/shop/IMG_6003.png';
+import pht2 from '@shared/images/shop/IMG_5997.png';
 
 export const ShopMainPage = () => {
     return (
-        <div>
+        <div className={styles.main_cointainer}>
             <div className={styles.container}>
                 <div className={styles.background}></div>
                 <p className={styles.title}>Filter.dept</p>
             </div>
 
-            <div className={styles.color1}></div>
-            <div className={styles.color2}></div>
-            <div className={styles.color3}></div>
-            <div className={styles.color4}></div>
-            <div className={styles.color5}></div>
-            <div className={styles.color6}></div>
+            <div className={styles.information}>
+                <div className={styles.photo_container}>
+                    <img src={pht1} className={styles.photo}/>
+                    <img src={pht2} className={styles.photo}/>
+                </div>
 
+                <div className={styles.text_container}>
+                    <p>THE NEW</p>
+                    <p>WO-MEN COLLECTION</p>
+                    <p>SUMMER 2025</p>
+                </div>
+            </div>
         </div>
     )
 }
