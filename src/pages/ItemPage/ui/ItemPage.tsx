b6896770f8f82d513f23538da62b303e3cf2b43f
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-09-12 20:55:03 +0300

    added new header

--- src/pages/ItemPage/ui/ItemPage.tsx
+++ src/pages/ItemPage/ui/ItemPage.tsx
@@ -1,8 +1,10 @@
 import styles from './item.module.scss'
+import {Shortheader} from "@/widgets";
 
 export const ItemPage = () => {
     return(
         <>
+            <Shortheader />
         </>
     )
 }
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,3 +2,4 @@ export { FooterShop } from './footer/forShop/ui/FooterShop.tsx';
 export { FooterFilter } from './footer/forFilter/ui/FooterFilter.tsx';
 export { HeaderFilter } from './header/forFilter/ui/HeaderFilter.tsx';
 export { HeaderShop } from './header/forShop/ui/HeaderShop.tsx';
+export { Shortheader } from './header/forShop/shortHeader/Shortheader.tsx';
