58d32e5f086b6c317f72be2aeab533e6581c1721
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-09-16 10:46:35 +0300

    added new styles

--- src/pages/ItemPage/ui/ItemPage.tsx
+++ src/pages/ItemPage/ui/ItemPage.tsx
@@ -13,14 +13,14 @@ export const ItemPage = () => {
                     </div>
 
                     <div className={styles.right}>
-                        <p>Shrimp Logo Boxy T-Shirt in soft white cotton jersey for Women is from look 14 of the Shrimp Collection.</p>
-                        <p><b>SIZE:</b> XS/S/M/L/XL</p>
-                        <p><b>MANUFACTURER:</b> Russia</p>
-                        <p><b>FABRIC WEIGHT:</b> 180 g/m²</p>
-                        <p><b>CARE INSTRUCTIONS:</b> Machine wash cold, do not bleach, tumble dry low, iron on reverse side if needed.</p>
-                        <p><b>AVAILABLE AT:</b> 12 Baumana Street, Kazan / 8 Pushkina Street, Kazan</p>
+                        <p className={styles.desc}>Shrimp Logo Boxy T-Shirt in soft white cotton jersey for Women is from look 14 of the Shrimp Collection.</p>
+                        <p className={styles.values}>SIZE: XS/S/M/L/XL</p>
+                        <p className={styles.values}>MANUFACTURER: Russia</p>
+                        <p className={styles.values}>FABRIC WEIGHT: 180 g/m²</p>
+                        <p className={styles.values}>CARE INSTRUCTIONS: Machine wash cold, do not bleach, tumble dry low, iron on reverse side if needed.</p>
+                        <p className={styles.values}>AVAILABLE AT: 12 Baumana Street, Kazan / 8 Pushkina Street, Kazan</p>
 
-                        <h3>Shrimp collection/T-shirts/women/</h3>
+                        <h3 className={styles.name}>Shrimp collection/T-shirts/women/</h3>
                     </div>
                 </div>
             </main>
