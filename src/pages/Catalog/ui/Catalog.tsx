6d218d95d1a688225b1fc491af16d64bf704310a
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-09-24 12:59:47 +0300

    started new page

--- src/pages/Catalog/ui/Catalog.tsx
+++ src/pages/Catalog/ui/Catalog.tsx
@@ -1,7 +1,17 @@
-import styles from './catalog.module.scss'
+import styles from './catalog.module.scss';
+import mainPhoto from '@shared/images/shop/catalog_main_photo.png'
 
 export const Catalog = () => {
     return(
-        <></>
+        <div className={styles.container}>
+            <div className={styles.logo_container}>
+                <img
+                    src={mainPhoto}
+                    className={styles.photo}
+                />
+
+                <p className={styles.logo}>Filter.dept</p>
+            </div>
+        </div>
     )
 }
