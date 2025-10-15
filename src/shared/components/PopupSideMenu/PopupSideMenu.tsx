e3589327975ebea6904b348a6931e8bdec791495
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-10-15 08:58:38 +0300

    added popup

--- src/shared/components/PopupSideMenu/PopupSideMenu.tsx
+++ src/shared/components/PopupSideMenu/PopupSideMenu.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss';
 export const PopupSideMenu = () => {
     return(
         <div className={styles.container}>
-            SideMenu
+            <p className={styles.title}>фильтр</p>
         </div>
     )
 }
