633800fd459dddf1bc606a3ff277969c4f5424f7
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-10-18 19:32:37 +0300

    popup

--- src/shared/components/PopupSideMenu/PopupSideMenu.tsx
+++ src/shared/components/PopupSideMenu/PopupSideMenu.tsx
@@ -17,6 +17,29 @@ export const PopupSideMenu = ({ onClose }: PopupSideMenuProps) => {
 
             <div className={styles.content}>
                 <p className={styles.title}>фильтр</p>
+
+                <div className={styles.filter_container}>
+                    <section>
+                        <p className={styles.title}>цвет</p>
+                    </section>
+
+                    <section>
+                        <p className={styles.title}>тип</p>
+                    </section>
+
+                    <section>
+                        <p className={styles.title}>коллекция</p>
+                    </section>
+
+                    <section>
+                        <p className={styles.title}>размер</p>
+                    </section>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button className={styles.button}>Очистить</button>
+                    <button className={styles.button}>Применить</button>
+                </div>
             </div>
         </div>
     );
