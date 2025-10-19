eac45de6c8e6923360a1102fb735527744693b77
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-10-19 10:38:44 +0300

    added

---
+++ src/widgets/CollorList/CollorList.tsx
@@ -0,0 +1,22 @@
+import styles from './collor.module.scss';
+
+export const CollorList = () => {
+    return(
+        <div>
+            <span className={styles.color1}></span>
+            <span className={styles.color2}></span>
+            <span className={styles.color3}></span>
+            <span className={styles.color4}></span>
+            <span className={styles.color5}></span>
+            <span className={styles.color6}></span>
+            <span className={styles.color7}></span>
+            <span className={styles.color8}></span>
+            <span className={styles.color9}></span>
+            <span className={styles.color10}></span>
+            <span className={styles.color11}></span>
+            <span className={styles.color12}></span>
+            <span className={styles.color13}></span>
+            <span className={styles.color14}></span>
+        </div>
+    )
+}
