e1167d7989804538056e12738638472bdcd21b5b
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-11-13 10:21:55 +0300

    changes

--- src/shared/components/SearchPopup/SearchPopup.tsx
+++ src/shared/components/SearchPopup/SearchPopup.tsx
@@ -1,24 +1,26 @@
-import { useState } from "react";
+import { useState, type FC } from "react";
 import styles from "./search.module.scss";
 import closeIcon from "@shared/images/popup/closeIcon.png";
 
-type PopupProps = {
+type SearchPopupProps = {
     onClose: () => void;
 };
 
-export const SearchPopup = ({ onClose }: PopupProps) => {
-    const [active, setActive] = useState<string | null>(null);
+export const SearchPopup: FC<SearchPopupProps> = ({ onClose }) => {
+    const [activeQuery, setActiveQuery] = useState<string | null>(null);
 
     return (
         <>
             <div className={styles.overlay} onClick={onClose} />
 
             <div className={styles.container}>
-                <div className={styles.input_container}>
+                <div className={styles.inputContainer}>
                     <input
-                        placeholder="что ищете"
                         type="text"
+                        placeholder="Что ищете"
                         className={styles.input}
+                        value={activeQuery ?? ""}
+                        onChange={(e) => setActiveQuery(e.target.value)}
                     />
                     <img
                         src={closeIcon}
@@ -29,13 +31,19 @@ export const SearchPopup = ({ onClose }: PopupProps) => {
                 </div>
 
                 <div className={styles.content}>
-                    <p className={styles.title}>чаще всего ищут</p>
+                    <p className={styles.title}>Чаще всего ищут</p>
 
-                    <div className={styles.filter_container}>
-                        <p className={styles.title}>футболка  Filter Everything</p>
-                        <p className={styles.title}>весна 2025</p>
-                        <p className={styles.title}>Ryfo uip hip hop</p>
-                        <p className={styles.title}>футболка  Filter Everything</p>
+                    <div className={styles.filterContainer}>
+                        {[
+                            "футболка Filter Everything",
+                            "весна 2025",
+                            "Ryfo uip hip hop",
+                            "футболка Filter Everything",
+                        ].map((item, index) => (
+                            <p key={index} className={styles.title}>
+                                {item}
+                            </p>
+                        ))}
                     </div>
                 </div>
             </div>
